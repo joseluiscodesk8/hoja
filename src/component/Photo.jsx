d0d12bcf5c2f458a338c3aa1a18ec9219541b03d
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-06-20 19:51:54 -0500

    photo2

--- src/component/Photo.jsx
+++ src/component/Photo.jsx
@@ -1,9 +1,24 @@
 import styles from "../styles/index.module.scss";
+import { motion } from "framer-motion";
 const Photo = () => {
   return (
     <>
       <picture className={styles.Photo}>
-        <img src="/jl2.png" alt="photo" />
+        <motion.img
+          src="/jl2.png"
+          alt="photo"
+          drag
+          dragConstraints={{
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+          }}
+          dragElastic={2}
+          whileDrag={{
+            boxShadow: "none",
+          }}
+        />
       </picture>
     </>
   );
